2103b17d3a7dc855f05853bec524b5b6c09e00f2
Christian Coulon <cdcoulon@ucdavis.edu>
2023-03-14 21:16:36 -0700

    fixes bad import name

--- test/nm-person.service.test.ts
+++ test/nm-person.service.test.ts
@@ -1,15 +1,19 @@
 import { describe, expect, test, jest } from '@jest/globals';
 
-import { PersonService } from '../src/service/nm-person.service';
+import { NmPersonService } from '../src/service/nm-person.service';
 
 jest.setTimeout(20000);
 
 describe('nm-person.service', () => {
     test('make sure we can get a column range', async () => {
-        expect(PersonService.getColumnRangeName('DISCORD_ID')).toBe('person!A');
-        expect(PersonService.getColumnRangeName('STATUS', 3)).toBe('person!B3');
-        expect(PersonService.getColumnRangeName('EMAIL')).toBe('person!D');
-        expect(PersonService.getColumnRangeName('EMAIL', 1, 'F')).toBe(
+        expect(NmPersonService.getColumnRangeName('DISCORD_ID')).toBe(
+            'person!A'
+        );
+        expect(NmPersonService.getColumnRangeName('STATUS', 3)).toBe(
+            'person!B3'
+        );
+        expect(NmPersonService.getColumnRangeName('EMAIL')).toBe('person!D');
+        expect(NmPersonService.getColumnRangeName('EMAIL', 1, 'F')).toBe(
             'person!D1:F'
         );
     });
