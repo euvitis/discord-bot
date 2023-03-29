3b0752c05784a487edd79eac23fbfbe1720fe402
Christian Coulon <cdcoulon@ucdavis.edu>
2023-03-29 16:21:25 -0700

    fix stupid weird ts problem with Messagemanager type

--- src/events/food-count-response.event.ts
+++ src/events/food-count-response.event.ts
@@ -3,7 +3,8 @@ import {
     TextChannel,
     ButtonInteraction,
     Interaction,
-    MessageManager
+    MessageManager,
+    TextBasedChannel
 } from 'discord.js';
 import { FoodCountInputCache } from './food-count-input.event';
 
@@ -27,8 +28,7 @@ export const FoodCountResponseEvent = async (interaction: Interaction) => {
     // here we can use that first action name to do different stuff
     // depending on what button it is
     if (idName === 'food-count-cancel') {
-        const m = interaction.channel
-            ?.messages as unknown as MessageManager<true>;
+        const m = (interaction.channel as TextBasedChannel)?.messages;
         const cache = FoodCountInputCache.get(idCache);
 
         if (!cache) {
