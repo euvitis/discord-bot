5c14849e2aa4add59b218b543dd6b08dc78f4031
Christian Coulon <cdcoulon@ucdavis.edu>
2023-04-06 06:45:05 -0700

    adds some notes to env config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,9 @@
+// TODO: this can be merged with nm-config.service
+// the main difference is that is an async wrapper for secrets manager
+// while these config aren't secret, but there is no reason not to
+// roll them together I don't think - keeps it simple
+
+// right now dev and test are the same
 type Env = 'dev' | 'test' | 'prod';
 
 interface Config {
@@ -14,13 +20,14 @@ const EnvConfig: {
         GSPREAD_CORE_ID: '17-BwSUuXOD_mawagA_cEjP9kVkWCC_boCUV_FikeDek',
         GSPREAD_INVENTORY_ID: '1uHQ6oL84fxlu3bXYxPIU7s1-T2RX0uWzCNC1Hxs8sMM'
     },
+    // these point to TEST data in night-tech folder
     test: {
-        GSPREAD_CORE_ID: '17-BwSUuXOD_mawagA_cEjP9kVkWCC_boCUV_FikeDek',
-        GSPREAD_INVENTORY_ID: '1uHQ6oL84fxlu3bXYxPIU7s1-T2RX0uWzCNC1Hxs8sMM'
+        GSPREAD_CORE_ID: '1y27iAsVWOG_l3yfLEvVIyKqKlL9i571pZN6wegCK_98',
+        GSPREAD_INVENTORY_ID: '18TujYCUGf4Lko-8VVJtyagmk2SNEouxTTde5opG1eoo'
     },
     prod: {
-        GSPREAD_CORE_ID: '17-BwSUuXOD_mawagA_cEjP9kVkWCC_boCUV_FikeDek',
-        GSPREAD_INVENTORY_ID: '1uHQ6oL84fxlu3bXYxPIU7s1-T2RX0uWzCNC1Hxs8sMM'
+        GSPREAD_CORE_ID: '1y27iAsVWOG_l3yfLEvVIyKqKlL9i571pZN6wegCK_98',
+        GSPREAD_INVENTORY_ID: '18TujYCUGf4Lko-8VVJtyagmk2SNEouxTTde5opG1eoo'
     }
 };
 if (!EnvConfig[process.env.NODE_ENV as Env]) {
